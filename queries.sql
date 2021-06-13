912181c7aaf776f632a1b6956549f3dbdff4235d
zacharyrb99 <zach.boudreaux999@gmail.com>
2021-06-12 23:03:43 -0400

    finished

--- queries.sql
+++ queries.sql
@@ -1,1 +1,7 @@
 -- write your queries here
+--Query 1--
+SELECT * FROM owners FULL JOIN vehicles ON owners.id = vehicles.owner_id;
+--Query 2--
+SELECT first_name, last_name, COUNT(owner_id) FROM owners JOIN vehicles ON owners.id = vehicles.owner_id GROUP BY (first_name, last_name) ORDER BY first_name;
+--Query 3 --
+SELECT first_name, last_name, AVG(price) AS average_price, COUNT(owner_id) FROM owners JOIN vehicles ON owners.id = vehicles.owner_id GROUP BY (first_name, last_name) HAVING AVG(price) > 10000 AND COUNT(owner_id) > 1 ORDER BY first_name DESC;
